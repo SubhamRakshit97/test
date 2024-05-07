d5188d37d427c8dd18faafe67429a4737607b0ac
Subham Rakshit <111345784+SubhamRakshit97@users.noreply.github.com>
2024-05-08 04:28:20 +0530

    Update settings.py

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware’,
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
